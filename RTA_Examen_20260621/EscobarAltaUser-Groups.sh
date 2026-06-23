89c30c9d3d7c6a21ab772f31803363b751f5b8e8
blacktio <alexescobar663@gmail.com>
2026-06-23 03:43:34 +0000

    Entrega Completa

---
+++ RTA_Examen_20260621/EscobarAltaUser-Groups.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Parámetros recibidos
+USUARIO_CLAVE=$1
+LISTA=$2
+
+# Obtener la clave 
+CLAVE=$(sudo grep "^${USUARIO_CLAVE}:" /etc/shadow | cut -d: -f2)
+
+# Guardar IFS y cambiarlo para leer línea por línea
+ANT_IFS=$IFS
+IFS=$'\n'
+
+for LINEA in $(cat $LISTA | grep -v ^#)
+do
+    # Extraer los datos con awk usando el separador ','
+    USUARIO=$(echo $LINEA | awk -F ',' '{print $1}')
+    GRUPO=$(echo $LINEA | awk -F ',' '{print $2}')
+    HOME_DIR=$(echo $LINEA | awk -F ',' '{print $3}')
+
+    # Crear grupo y usuario
+    sudo groupadd $GRUPO
+    sudo useradd -m -s /bin/bash -g $GRUPO -d $HOME_DIR -p "$CLAVE" $USUARIO
+done
+
+# Restaurar IFS
+IFS=$ANT_IFS
